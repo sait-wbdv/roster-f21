df6d8214451c1ae5a14c4f3ef80f8a1af1089dc6
Ashlyn <lilyx13@tutanota.com>
2021-12-10 16:23:05 -0700

    card bindings prepped

--- components/Card.vue
+++ components/Card.vue
@@ -1,3 +1,26 @@
 <template>
-  
+  <figure>
+    <nuxt-img :src="person.image" />
+    <figcaption>
+      <h3>{{ person.name }}</h3>
+      <p>{{ person.tagline }}</p>
+      <ul>
+        <li v-for="social in person.socials" :key="social.id">
+          <a :href="social.link" v-html="social.label"></a>
+        </li>
+      </ul>
+    </figcaption>
+  </figure>
 </template>
+<script>
+export default {
+  props: {
+    person: {
+      type: Object,
+    },
+  },
+};
+</script>
+<style scoped>
+
+</style>
